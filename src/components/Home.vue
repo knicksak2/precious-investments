913df165f247841232bfb81b791a8f1dad13fd41
Alan Krome <alan.krome@bamtechmedia.com>
2020-01-14 16:51:47 -0500

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-container class="grey lighten-5">
+        <v-row>
+            <v-col
+                cols="12"
+                sm="6"
+                md="8"
+            >
+                <v-img src="../assets/how-write-annual-finance-reports.jpg" />
+            </v-col>
+            <v-col
+                cols="6"
+                md="4"
+            >
+                <div class="my-2">
+                    <v-btn large color="primary">Download Now</v-btn>
+                </div>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+ 
+ <script>
+ export default {
+     name: 'Home'
+ }
+ </script>
+ 
